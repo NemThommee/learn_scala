4f5248aa0c4d636b3b733cb2d36c64e245461350
Thommee <hu.nemtom@gmail.com>
2018-09-05 17:03:36 +0200

    Leágaztam és a 6-os osztók is meglettek

--- src/scala1.scala
+++ src/scala1.scala
@@ -13,9 +13,10 @@ object CurryTest extends App {
 
   def modN( n: Int)( x: Int)= ( x % n) == 0
 
-  val nums = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)
+  val nums = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
   print( "2-vel oszthatók  : ")  ; println( filter( nums, modN(2)))
   print( "3-mal oszthatók  : ")  ; println( filter( nums, modN(3)))
   print( "4-gyel oszthatók : ")  ; println( filter( nums, modN(4)))
   print( "5-tel oszthatók  : ")  ; println( filter( nums, modN(5)))
+  print( "6-tal oszthatók  : ")  ; println( filter( nums, modN(6)))
 }
