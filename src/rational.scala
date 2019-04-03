042c3d98379d04ea05c8e0b5035eb66eb5dc1dd2
Tamas Nemeth <42572737+NemThommee@users.noreply.github.com>
2019-04-03 09:29:21 +0200

    Update rational.scala

--- src/rational.scala
+++ src/rational.scala
@@ -5,7 +5,7 @@ class Rational(n: Int, d: Int) {
   val denom = d / g
   def this(n: Int) = this(n, 1)
   def +(that: Rational) : Rational = new Rational(numer * that.denom + that.numer * denom,	denom * that.denom)
-	def *(that: Rational) : Rational = new Rational(numer * that.numer, denom * that.denom) 
+  def *(that: Rational) : Rational = new Rational(numer * that.numer, denom * that.denom) 
   override def toString = numer + "/" + denom
   private def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
